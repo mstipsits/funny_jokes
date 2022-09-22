e3678bb6cba80d11f377f71400351977e7b78dda
Michael Stipsits <michael.stipists@gmx.at>
2022-09-22 20:07:38 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,29 @@ menuList.style.maxHeight = "0px";
 let menuIcon = document.getElementById("menuIcon");
 menuIcon.addEventListener("click", () => toggleMenu(menuList));
 
+let randomDog = document.getElementById("randomDog");
+if (randomDog) {
+    fetchRandomDog(randomDog)
+}
+
 function toggleMenu(menuList) {
     if (menuList.style.maxHeight === "0px") {
         menuList.style.maxHeight = "130px";
     } else {
         menuList.style.maxHeight = "0px";
     }
+}
+
+function fetchRandomDog(randomDog) {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            let dogImgUrl = data.message;
+            let dogImg = document.createElement("img")
+            dogImg.setAttribute('src', `${dogImgUrl}`)
+            dogImg.setAttribute('width', '340px');
+            dogImg.setAttribute('alt', 'Picture of random Dog');
+            randomDog.appendChild(dogImg);
+        })
+        .catch(err => console.log(err))
 }
